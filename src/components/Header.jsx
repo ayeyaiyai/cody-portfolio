784509ddd5d13265cf87681d90be3d5e60724335
ayeyaiyai <105176518+ayeyaiyai@users.noreply.github.com>
2023-11-09 16:08:31 -0500

    Add scroll on click

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,38 @@
+import React from 'react';
 import '../styles/Header.css';
 
 function Header() {
-    return (
-        <section className="header">
-            <div className='header-body'>
-                <div className='header-name'>Cody Gilbert</div>
-                <div className='header-links'>
-                    <span className='header-link'>About</span>
-                    <span className='header-link'>Research</span>
-                    <span className='header-link'>Teaching</span>
-                    <span className='header-link'>Services</span>
-                </div>
-            </div>
-        </section>
-    )
+  const scrollToSection = (sectionId) => {
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
+    }
+  };
+
+  return (
+    <section className='header'>
+      <div className='header-body'>
+        <div className='header-name'>Cody Gilbert</div>
+        <div className='header-links'>
+          <span onClick={() => scrollToSection('about-section')} className='header-link'>
+            About
+          </span>
+          <span onClick={() => scrollToSection('research-section')} className='header-link'>
+            Research
+          </span>
+          <span onClick={() => scrollToSection('teaching-section')} className='header-link'>
+            Teaching
+          </span>
+          <span onClick={() => scrollToSection('services-section')} className='header-link'>
+            Services
+          </span>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Header;
