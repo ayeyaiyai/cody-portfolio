01a321b0b8f236dee1ad617a7163628cd1b3af23
ayeyaiyai <105176518+ayeyaiyai@users.noreply.github.com>
2023-11-09 16:52:08 -0500

    Adjust scroll function

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -17,16 +17,16 @@ function Header() {
       <div className='header-body'>
         <div className='header-name'>Cody Gilbert</div>
         <div className='header-links'>
-          <span onClick={() => scrollToSection('about-section')} className='header-link'>
+          <span onClick={() => scrollToSection('about')} className='header-link'>
             About
           </span>
-          <span onClick={() => scrollToSection('research-section')} className='header-link'>
+          <span onClick={() => scrollToSection('research')} className='header-link'>
             Research
           </span>
-          <span onClick={() => scrollToSection('teaching-section')} className='header-link'>
+          <span onClick={() => scrollToSection('teaching')} className='header-link'>
             Teaching
           </span>
-          <span onClick={() => scrollToSection('services-section')} className='header-link'>
+          <span onClick={() => scrollToSection('services')} className='header-link'>
             Services
           </span>
         </div>
